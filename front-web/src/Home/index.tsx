0769ddbb8d6e9ec11e9db3abf7156c48eca4da79
Eduardo Correia Pinto Alves Barbosa <eduardo@localhost.localdomain>
2021-01-09 14:05:39 -0300

    Add routes

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './styles.css';
 import { ReactComponent as MainImage } from './mainImage.svg';
-import Footer from '../Footer'
+import { Link } from "react-router-dom";
 
 function Home() {
 
@@ -17,16 +17,15 @@ function Home() {
                             Escolha seu pedido e em poucos minutos <br />
                         levaremos na sua porta.
                     </h3>
-                        <a href="orders" className="home-btn-order">
+                        <Link to="/orders" className="home-btn-order">
                             FAZER PEDIDO
-                    </a>
+                        </Link>
                     </div>
                     <div className="home-image">
                         <MainImage />
                     </div>
                 </div>
             </div>
-            <Footer />
         </>
     )
 }
---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './styles.css';
+
+function Orders() {
+    
+    return(
+        <>
+            Orders
+        </>
+    )
+}
+
+export default Orders;
