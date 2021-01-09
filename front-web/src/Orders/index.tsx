21b9f36a4b5169d88577061d1379c3b7fbf75a1c
Eduardo Correia Pinto Alves Barbosa <eduardo@localhost.localdomain>
2021-01-09 17:10:00 -0300

    Add ProductList and ProductCard in Orders

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import ProductsList from "./ProductsList";
 import StepHeaders from "./StepHeaders";
 import './styles.css';
 
 function Orders() {
     
     return(
-        <>
+        <div className="orders-container">
             <StepHeaders />
-        </>
+            <ProductsList />
+        </div>
     )
 }
 
